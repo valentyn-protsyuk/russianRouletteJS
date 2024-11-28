87cefcaf22b24dac1c11a4ee66cd64079f010335
valentyn_protsyuk <valentyn.protsyuk@yahoo.com>
2024-11-27 22:50:55 -0800

    changing states function

--- gameLoop.js
+++ gameLoop.js
@@ -13,3 +13,103 @@ $.getJSON( "data/game.json", function( data ) {
         next_state('loading'); 
 
 });
+
+function next_state( state) {
+    //update current state
+    console.log("Current State = " + current_state + " --> New State= " + state) 
+    current_state = state
+    
+    
+    //6 turns max
+    if(turns === 6){
+        current_state = finalTurn(current_state);
+        turns = 0;
+    }
+
+    //reset turns
+    if (game_data['states'][ current_state ]['turns_reset'] != null){
+        turns = 0;
+    }
+
+    //count turns
+    if (game_data['states'][ current_state ]['turn_count'] != null){
+        console.log('turn'+turns+':'+current_state);
+        turns += 1;
+    }
+
+    //change btn labels
+    if (game_data['states'][ current_state ]['change_label'] != null){
+        btn_change(game_data['states'][ current_state ]['change_label']);
+    }
+
+    //stim change
+    if (game_data['states'][ current_state ]['stim_change'] != null){
+            console.log("+ stim " + game_data['states'][ current_state ]['stim_change'] ) 
+            stim = stim + game_data['states'][ current_state ]['stim_change']
+    }
+
+    //dope change
+    if (game_data['states'][ current_state ]['dope_change'] != null){
+        console.log("+ dope " + game_data['states'][ current_state ]['dope_change'] ) 
+        dope = dope + game_data['states'][ current_state ]['dope_change']
+    }
+
+    //totals
+    if(game_data['states'][ current_state ]['print_totals'] != null){
+        printTotal();
+        stim = 0;
+        dope = 0;
+    }
+
+    //sound
+    if (game_data['states'][ current_state ]['play_sound'] != null){
+        audioElement.setAttribute('src', game_data['states'][ current_state ]['play_sound']);
+        audioElement.play();
+    }
+    
+    //if there is text in player state display it
+    if (game_data['states'][ current_state ]['text'] != null){
+        //console.log("Header: " + game_data['states'][ current_state ]['text']);
+        $('#game_text').html( game_data['states'][ current_state ]['text'] );
+    }
+    
+    //if there is gif in player state display it
+    if (game_data['states'][ current_state ]['image_change'] != null){
+            $("#game_img").html( game_data['states'][ current_state ]['image_change'] );
+    }
+    
+    //display buttons if needed
+    if (game_data['states'][ current_state ]['show_btns'] != null){
+        btn_show(game_data['states'][ current_state ]['show_btns']);
+    }
+    //transition states
+    else {
+        btn_hide();
+
+        //if there is no delay call new state
+        if (game_data['states'][ current_state ]['delay'] == null){
+            if ( typeof game_data['states'][current_state]['next_state'] == "string"){
+                next_state(game_data['states'][current_state]['next_state'])
+            }
+            //random next state
+            else{
+                next_state(pick_a_winner(game_data['states'][current_state]['next_state']));
+            }
+        }
+        //if there is delay call timeout
+        else{
+            if (game_data['states'][ current_state ]['show_text'] != null){
+                $('#game_text').html( game_data['states'][ current_state ]['show_text'])
+            }
+            setTimeout(function() {
+                if ( typeof game_data['states'][current_state]['next_state'] == "string"){
+                    next_state(game_data['states'][current_state]['next_state']);
+                }
+                //random next state
+                else{
+                    next_state(pick_a_winner(game_data['states'][current_state]['next_state']));
+                } 
+            }, game_data['states'][ current_state ]['delay']);
+        }
+    }
+}
